bd5b5e9d6d53b3cb0d551635358676725ee786c6
Gregoire BEAUDEQUIN <gregoire.beaudequin@gmail.com>
2016-10-04 16:57:23 +0000

    missing double quotes

--- src/main/java/fr/xebia/clickcount/Configuration.java
+++ src/main/java/fr/xebia/clickcount/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
     public final int redisConnectionTimeout;  //milliseconds
 
     public Configuration() {
-        redisHost = redis;
+        redisHost = "redis";
         redisPort = 6379;
         redisConnectionTimeout = 2000;
     }
